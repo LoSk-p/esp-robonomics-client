2ad4f6f76c971b7255ff1a6f20461fc0de5f0524
LoSk-p <alki.spb@mail.ru>
2025-01-09 17:01:58 +0300

    fix functions definitions

--- src/Robonomics.h
+++ src/Robonomics.h
@@ -26,8 +26,8 @@ public:
     void generateAndSetPrivateKey();
     void setPrivateKey(uint8_t *privateKey);
     void setPrivateKey(const char* hexPrivateKey);
-    const char* sendDatalogRecord(std::string data);
-    const char* sendRWSDatalogRecord(std::string data, const char *owner_address);
+    const char* sendDatalogRecord(const std::string& data);
+    const char* sendRWSDatalogRecord(const std::string& data, const char *owner_address);
     const char* sendCustomCall();
     const char* getSs58Address() const;
     const char* getPrivateKey() const;
